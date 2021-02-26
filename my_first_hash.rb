3ba5b452051fd8dbcb874967f077c07c22ab4f23
Vince W <vincewallgren@me.com>
2021-02-26 04:11:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -40,5 +40,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-  return shipping_manifest 
+  return shipping_manifest
 end
